7553b93251bdd6072f1e969950311a8af41b1633
Robert Kaiser <>
2022-04-26 23:17:59 -0700

    Test0

---
+++ .idea/test/testRandom.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class testRandom {
+
+    @Test
+            public void randomTest(){
+        assertEquals(2, 2);
+    }
+
+}
